031ff819b2c7de9c119b8617bc9150949a82d1d2
Christopher Justine William <13519006@std.stei.itb.ac.id>
2021-06-12 19:53:26 +0700

    Fix bug

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,23 +1,21 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useRef, useEffect} from 'react'
 import '@tomtom-international/web-sdk-maps/dist/maps.css'
 import tt from '@tomtom-international/web-sdk-maps';
 import '../css/Map.css'
 
 const Map = () => {
   const mapElement = useRef();
-  const [map, setMap] = useState({});
 
   useEffect(() => {
-    let map = tt.map({
+    let mapData = tt.map({
       key: 'RRF5SbdgsyPTbw2a6yRBv1TAv7WTZ3RT',
       container: mapElement.current,
       center: [-121.91599, 37.36765],
       zoom: 15
     });
-    setMap(map);
-    new tt.Marker().setLngLat([-121.91599, 37.36765]).addTo(map);
-    return () => map.remove();
-  }, []);
+    new tt.Marker().setLngLat([-121.91599, 37.36765]).addTo(mapData);
+    return () => mapData.remove();
+  });
 
   return (
     <div ref={mapElement} className="mapDiv"></div>
